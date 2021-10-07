c0052f95db272ae635495f4226fe2013f7a09e3f
Pisit101 <pisit.lertpitaksit@gmail.com>
2021-10-07 23:28:40 +0700

    ฟฟ

--- databases/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-$pdo = new PDO("mysql:host=localhost;dbname=hotel", "root", "1234");
-$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-$pdo->exec ( "SET NAMES \"utf8\"" );
-?>
